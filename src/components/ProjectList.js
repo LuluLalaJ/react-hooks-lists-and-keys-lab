b3f2a52c10e66b85c4330c605d9249222bdd165f
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-10 17:27:10 -0400

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,23 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const renderProjectItems = projects.map( project => 
+      <ProjectItem 
+        key={ project.id }
+        {... project}
+    />)
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        { renderProjectItems }
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+  
